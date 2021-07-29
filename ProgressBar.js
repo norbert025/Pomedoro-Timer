5f82fde87543e9139edc2ed40affad970122fd4f
Norberto Gonzalez <36218596+norbert025@users.noreply.github.com>
2021-07-28 21:55:14 -0400

    Add files via upload

---
+++ ProgressBar.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+
+function ProgressBar({currentTime, totalTime}){
+    const progress = (currentTime / totalTime) * 100
+
+    return(
+        <div className="row mb-2">
+          <div className="col">
+            <div className="progress" style={{ height: "20px" }}>
+              <div
+                className="progress-bar"
+                role="progressbar"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                aria-valuenow={progress} // TODO: Increase aria-valuenow as elapsed time increases
+                style={{ width: `${progress}%` }} // TODO: Increase width % as elapsed time increases
+              />
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default ProgressBar
